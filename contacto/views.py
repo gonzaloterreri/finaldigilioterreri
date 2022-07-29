7b916eabf5cd7b51b7ea00226970011a604f0b2f
gonzaloterreri <napiaterreri@gmail.com>
2022-07-28 22:05:41 -0300

    contacto

--- contacto/views.py
+++ contacto/views.py
@@ -1,30 +1,38 @@
+from email.headerregistry import ContentDispositionHeader
 from django.shortcuts import render, redirect
 
 from .forms import FormularioContacto
 
-from django.core.mail import EmailMessage
+from django.template.loader import render_to_string
+
+
+
+from django.core.mail import send_mail
+
 
 
 # Create your views here.
 
 def contacto(request):
     formulario_contacto=FormularioContacto()
-
+    
     if request.method=="POST":
         formulario_contacto=FormularioContacto(data=request.POST)
         if formulario_contacto.is_valid():
             nombre=request.POST.get("nombre")
             email=request.POST.get("email")
             contenido=request.POST.get("contenido")
+           
 
-
-            email=EmailMessage("Mensaje desde App Django",
-            "El usuario con nombre {} con la dirección {} escribe lo siguiente:\n\n {}".format(nombre,email,contenido),
-            contenido,["cosmefulanitoweb499@hotmail.com"],reply_to=[email])
 
             try:
-                email.send()
-
+                asunto=email
+                mensaje=contenido
+                mensaje_texto="texto"
+                from_email="cosmefulanitoweb499@hotmail.com"
+                to="cosmefulanitoweb499@hotmail.com"
+                send_mail(asunto,mensaje_texto,from_email,[to], html_message=mensaje)
+                
                 return redirect("/contacto/?valido")
             except:
                 return redirect("/contacto/?novalido")
